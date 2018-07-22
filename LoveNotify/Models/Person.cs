eac09e42fd4912aabcf71b4701994671d70509a5
oufu99 <111>
2018-07-22 18:47:32 +0800

    aa

---
+++ LoveNotify/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SayLove.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Msg1 { get; set; }
+        public string Msg2 { get; set; }
+    }
+}
